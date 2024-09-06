de7d38734c03afb3eaa4a8b99bdc47e213df0c85
Bule <luca.bulon@student.uliege.be>
2024-09-06 20:32:15 +0200

    Update Compute_roll.m

--- Compute_roll.m
+++ Compute_roll.m
@@ -6,11 +6,10 @@ delta_t    = dx_roll;                          % Time interval to perform the ro
 
 
 p_dot        = delta_roll*2/delta_t^2;         % Scalar .Angular acceleration of the spacecraft (cst) [rad s-2], first phase by kinetic equation
-% fprintf("p_dot : %f \n",p_dot);
+
 T            = Ixx*p_dot;                      % Scalar .Torque of the spacecraft (cst) [Nm], first phase by euler's equation
 H_max        = T*dx_roll;                      % Scalar .Maximum angular momentum of the Wheels/spacecraft [Nm s], first phase
 
-fprintf("H_max : %f \n",H_max);
 w_max_Rspeed = 7000*2*pi/60;                   % Scalar .Maximum rotation speed of the wheel [rad s-1], first phase
 
 % initial time
@@ -81,13 +80,7 @@ Omega_dot    = Omega_max * 2 * pi/60 /dx_roll;    % Scalar .Wheel angular accele
 Iw_roll      = T/(2*Omega_dot*sin(beta));          % Scalar .Inertia of the wheel [kg m²], first phase
 % ? 
 
-if print_result
-    fprintf('>>------------Roll-----------<<\n');
-    fprintf('>> T:                %f [Nm] \n',T);
-    fprintf('>> H_max:            %f [Nms] \n',H_max);
-    fprintf('>> w_max_Rspeed:     %f [rad/s] \n',w_max_Rspeed);
-    fprintf('>> Iw:               %f [kgm^2] \n',Iw_roll);
-end
+
 
 % Angulaire acceleration of the wheels
 alpha1    = Omega_dot;                                                   % Scalar .Wheel angular acceleration [rad s-2], first phase
@@ -105,29 +98,7 @@ Omega1_t2  = Omega1_t1(end) - Omega_dot * (t_2 - t_1(end));  % Vector .Angular v
 Omega1     = [Omega1_t1, Omega1_t2];  
 
 
-if do_fig
-    figure ;
-    plot(t, Omega1, 'LineWidth', 2)
-    xlabel('Time [s]', 'Interpreter', 'latex');  % Utilisation de LaTeX pour l'axe des x
-    ylabel('Angular velocity [rad/s]', 'Interpreter', 'latex');  % Utilisation de LaTeX pour l'axe des y
-    grid on;
-    % Création du dossier si nécessaire
-    folder_path = 'figures/step_1_roll';  % Chemin du dossier
-    if ~exist(folder_path, 'dir')
-        mkdir(folder_path);  % Créer le dossier s'il n'existe pas
-    end
 
-    % Sauvegarde de la figure en PDF
-    save_path = fullfile(folder_path, 'angular_velocity_wheel_1_plot.pdf');  % Chemin complet du fichier de sauvegarde
-    % Ajuster les propriétés de la figure et du papier pour une meilleure sauvegarde
-    set(gcf, 'Units', 'Inches');
-    pos = get(gcf, 'Position');
-    set(gcf, 'PaperPositionMode', 'auto');
-    set(gcf, 'PaperUnits', 'Inches', 'PaperSize', [pos(3), pos(4)]);
-    
-    % Sauvegarder la figure en PDF
-    print(gcf, save_path, '-dpdf', '-bestfit');
-end
 % second weel
 
 Omega2_t1 = - Omega1_t1;                              % Vector .Angular velocity of the wheel 2 during the first phase [rad s-1]
@@ -138,30 +109,6 @@ Omega2    = [Omega2_t1, Omega2_t2];                   % Vector .Angular velocity
 
 Qf1 =  - c*(sin(beta)*p - Omega1);                    % Vector .Friction torque [Nm] using Nexton at spacecraft
 
-% Créer le plot
-if do_fig
-    figure();
-    hold on;
-    grid on;
-
-    % Premier axe y pour la tension
-    yyaxis left;
-    plot(t,Iw_roll * alpha, 'LineWidth', 2);
-    ylabel('p [V]', 'Interpreter', 'latex');
-
-    % Deuxième axe y pour la force contre-EMF
-    yyaxis right;
-    plot(t, Qf1, 'LineWidth', 2);
-    ylabel('Qf1 [A]', 'Interpreter', 'latex');
-
-    % Axe x pour le temps
-    xlabel('Time [s]', 'Interpreter', 'latex');
-
-    % Légendes
-    legend('Tension (p)', 'Force contre-EMF (Qf1)', 'Location', 'best');
-
-    hold off;
-end
 
 % Obtenir la taille
 [rows, cols] = size(Qf1);
@@ -182,6 +129,7 @@ if abs(max_value) < abs(min_value)
 else
     index = max_index ;
 end
+
 max_v = f_roll_values(index);
 voltage_circ = max_v - N*(sin(beta)*p(index) - Omega1(index));
 % fprintf(">>voltage_circ in roll : %f [kV] \n",abs(voltage_circ)/1000);
@@ -189,20 +137,31 @@ voltage_circ = max_v - N*(sin(beta)*p(index) - Omega1(index));
 max_p = (voltage_circ)^2/R/10^6;
 
 
-if print_result
-    fprintf(">> Maximum voltage in roll : %f [kV] \n",abs(max_v/10^3));
-    fprintf(">> Maximum power in roll :   %f [MW] \n",max_p);
-end
+% if print_result
+%     fprintf(">> Maximum voltage in roll : %f [kV] \n",abs(max_v/10^3));
+%     fprintf(">> Maximum power in roll :   %f [MW] \n",max_p);
+% end
+
+%========================%
+        % Modif %
+%========================%
+i = T_vec / (2*N*sin(beta));
+voltage = i * R;
+
+%max current
+i_max = max(abs(i));
+voltage_max = i_max * R;
+voltage_max = voltage_max / 1000; %  [kV]
+
+power_max = voltage_max * i_max;
+power_max = power_max / 1000; % [MW]
+
 
 if do_fig
     figure();
     hold on;
     grid on;
-    yyaxis left;
-    plot(t, e1, 'LineWidth', 2);
-    ylabel('Armature voltage [V]    ', 'Interpreter', 'latex');
-    yyaxis right;
-    plot(t, i1, 'LineWidth', 2);
+    plot(t, i, 'LineWidth', 2);
     ylabel('Electrical current [A]', 'Interpreter', 'latex');
     xlabel('Time [s]', 'Interpreter', 'latex');
     hold off;
@@ -211,7 +170,7 @@ if do_fig
     if ~exist(folder_path, 'dir')
         mkdir(folder_path);  % Créer le dossier s'il n'existe pas
     end
-    save_path = fullfile(folder_path, 'current_voltage_plot.pdf');  % Chemin complet du fichier de sauvegarde
+    save_path = fullfile(folder_path, 'current_plot.pdf');  % Chemin complet du fichier de sauvegarde
         
     % Ajuster les propriétés de la figure et du papier pour une meilleure sauvegarde
     set(gcf, 'Units', 'Inches');
@@ -222,5 +181,16 @@ if do_fig
     % Sauvegarder la figure en PDF
     print(gcf, save_path, '-dpdf', '-bestfit');
 end
-
+voltage_t0 = voltage(1) / 1000;
+if print_result
+    fprintf('>>------------Roll-----------<<\n');
+    fprintf('>> T:                %f [Nm] \n',T);
+    fprintf('>> H_max:            %f [Nms] \n',H_max);
+    fprintf('>> w_max_Rspeed:     %f [rad/s] \n',w_max_Rspeed);
+    fprintf('>> Iw:               %f [kgm^2] \n',Iw_roll);
+    fprintf('>> Current at t = 0: %f [A] \n', i(1));
+    fprintf('>> Voltage at t = 0: %f [KV] \n', voltage_t0);
+    fprintf('>> Max voltage: %f [KV] \n', voltage_max);
+    fprintf('>> Max power: %f [MW] \n', power_max);
+end
 end
